6abaea787c2d0bc5f6f27e1a3a9ae0ee86e1d9c6
emirvine <emilymirvine@gmail.com>
2016-07-27 06:21:11 -0400

    Updating documentation style format

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = []
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -131,7 +131,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
